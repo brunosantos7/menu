09057472c5033c2ecd15ddf7adf9a530147f7439
Bruno da Silveira Santos <bsantos@synvata.com>
2020-01-10 10:14:05 -0300

    first commit

---
+++ src/main/java/br/com/menu/menudigital/MenudigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.menu.menudigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenudigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenudigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/menu/menudigital/MenudigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.menu.menudigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenudigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
