2b3ac5ec6a7c95fe9946ef20d8c1a4851f9b6af4
Bruno da Silveira Santos <bsantos@synvata.com>
2020-01-12 22:10:27 -0300

    Adding swagger on the project to document the APIs.

--- src/main/java/br/com/menu/menudigital/MenudigitalApplication.java
+++ src/main/java/br/com/menu/menudigital/MenudigitalApplication.java
@@ -3,7 +3,11 @@ package br.com.menu.menudigital;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 @SpringBootApplication
+@EnableSwagger2
 public class MenudigitalApplication {
 
 	public static void main(String[] args) {
